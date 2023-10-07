5ecf238486e6d5f032dd99a4eb5ab273bdbfc111
Dbataillard <damiebtd@gmail.com>
2023-10-08 00:51:38 +0200

    2nd commit

--- index.py
+++ index.py
@@ -4,13 +4,13 @@ from airtable import Airtable
 app = Flask(__name__)
 
 # Configuration Airtable
-API_KEY = 'patdFafREOKHC0KpG.9d1e1a38bdbe06fef04c641e040004da55bcba770bb7849051c87c2a666c01cb'
-BASE_ID = 'appIUsjqTXp0S6dEk'
+API_KEY = 'pat28jA9jpapynge9.d82b2274baf802746434350cb6b73d4fd2d29caaeeccd2ab68be5cb3f43405b6'
+BASE_ID = 'appVGsF9U4djrIPsz'
 CLIENT_TABLE = 'Leads'
 CORDAGE_TABLE = 'Products'
 
-airtable_clients = Airtable(BASE_ID, CLIENT_TABLE, api_key=API_KEY)
-airtable_cordages = Airtable(BASE_ID, CORDAGE_TABLE, api_key=API_KEY)
+airtable_clients = Airtable(BASE_ID,CLIENT_TABLE, API_KEY)
+airtable_cordages = Airtable(BASE_ID,CORDAGE_TABLE, API_KEY)
 
 @app.route('/')
 def index():
@@ -21,14 +21,19 @@ def index():
 def add_client():
     data = {
         'Nom': request.form['nom'],
-        'Email': request.form['email']
+        'Prénom' : request.form['prenom'],
+        'Email': request.form['email'],
+        'Ville': request.form['ville'],
+        'Code postal': request.form['codepostal'],
+        'Genre': request.form['genre'],
+        'Date de naissance': request.form['datedenaissance']
     }
     airtable_clients.insert(data)
     return redirect(url_for('index'))
 
 @app.route('/choose_cordage', methods=['POST'])
 def choose_cordage():
-    cordage_id = request.form['cordage_id']
+    cordage_id = request.form['Marque']
     date_recuperation = request.form['date_recuperation']
     # Ajoutez le code pour enregistrer le choix du client dans Airtable
     return redirect(url_for('index'))
